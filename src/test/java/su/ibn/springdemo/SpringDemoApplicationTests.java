4402b454a0afe5098f9f4f14a1fe97eca4596d14
Andrei Vvedenskii <andrei.vvedenskii@netcracker.com>
2022-03-17 12:23:04 +0400

    initial commit

---
+++ src/test/java/su/ibn/springdemo/SpringDemoApplicationTests.java
@@ -0,0 +1,12 @@
+package su.ibn.springdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
