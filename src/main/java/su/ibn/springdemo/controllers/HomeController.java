be452e5065c7497b55e5816f627e94c09ab36e7a
Andrei Vvedenskii <andrei.vvedenskii@netcracker.com>
2022-03-18 13:24:32 +0400

    * 2 lesson complete * add form for add new book

--- src/main/java/su/ibn/springdemo/controllers/HomeController.java
+++ src/main/java/su/ibn/springdemo/controllers/HomeController.java
@@ -3,12 +3,16 @@ package su.ibn.springdemo.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.sun.istack.NotNull;
+import su.ibn.springdemo.model.Book;
 import su.ibn.springdemo.repository.BookRepository;
 
 @Controller
+@RequestMapping("/")
 public class HomeController {
 
     private final BookRepository bookRepository;
@@ -24,11 +28,18 @@ public class HomeController {
 
 
 
-    @RequestMapping("/")
+    @GetMapping
     public String getIndex(@NotNull Model model) {
-
         model.addAttribute("books", bookRepository.findAll());
+        model.addAttribute("newBook", new Book());
         return "index";
     }
+
+    @PostMapping
+    public String createBook(Book book) {
+        bookRepository.save(book);
+        return "redirect:/";
+    }
+
 
 }
